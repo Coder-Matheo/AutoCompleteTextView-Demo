b3029ef9d04d3ecf875904094292748b2be47dbc
Coder-Matheo <ali.dinarvand1370@gmail.com>
2022-01-02 00:40:12 +0100

    History of Input field Name

---
+++ app/src/main/java/rob/autocomplatetextviewdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package rob.autocomplatetextviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    AutoCompleteTextView autoCompleteTextView;
+    MultiAutoCompleteTextView multiAutoCompleteTextView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        multiAutoCompleteTextView = findViewById(R.id.multiAutoCompleteTextView);
+        button = findViewById(R.id.button);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(adapter);
+
+        multiAutoCompleteTextView.setAdapter(adapter);
+        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+    }
+    private static  final String[] COUNTRIES = new String[]{
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+}
